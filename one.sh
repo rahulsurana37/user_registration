7d6feb8b51d1a6ebb6b8c301c20b8c3cd3d17970
unknown <rahulsurana37@gmail.com>
2020-06-16 17:00:42 +0530

    Added First name

--- one.sh
+++ one.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/bin/bash 
 
+shopt -s extglob
 echo "Welcome to User Registration Exercise"
+read -p "Enter First Name: " first
+if [[ $first =~ ^[A-Z]{1}+[a-z]{2,}$ ]]
+then
+	echo "Valid First Name";
+else
+	echo "Invalid First Name. Please Re-Enter!";
+fi
