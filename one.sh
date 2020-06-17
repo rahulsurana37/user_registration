33226460ac6d48256b3bf13bde19536b1e6ca5c4
unknown <rahulsurana37@gmail.com>
2020-06-17 14:31:01 +0530

    Added E-mail Verification

--- one.sh
+++ one.sh
@@ -4,11 +4,22 @@ shopt -s extglob
 echo "Welcome to User Registration Exercise"
 read -p "Enter First Name: " first
 read -p "Enter Second Name: " second
-pat = "^[A-Z]{1}+[a-z]{2,}$"
+name_pat="^[A-Z]{1}+[a-z]{2,}$"
 
-if [[ $first =~ $pat && $second =~ $pat ]]
+if [[ $first =~ $name_pat && $second =~ $name_pat ]]
 then
 	echo "Valid Name";
 else
 	echo "Invalid Name. Please Re-Enter!";
 fi
+
+read -p "Enter your E-mail ID: " email
+email_pat="^[a-zA-Z0-9]+/@[a-zA-Z]+/.[a-z.]{2,8}$"
+function email_check(){
+	if [[ $email =~ $email_pat ]]; then
+		echo "Email is Valid." $email
+	else
+		echo "$email - Email is Invalid. Please try again."
+	fi
+}
+email_check
