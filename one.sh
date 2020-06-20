a9fd8c0af152cce26233418d23d32525a6699d61
unknown <rahulsurana37@gmail.com>
2020-06-20 10:51:56 +0530

    Added Last name

--- one.sh
+++ one.sh
@@ -4,7 +4,7 @@ shopt -s extglob
 echo "Welcome to User Registration Exercise"
 read -p "Enter First Name: " first
 read -p "Enter Second Name: " second
-pat="^[A-Z]{1}+[a-z]{2,}$";
+pat="^[A-Z]{1}[a-z]{2,}$";
 
 if [[ $first =~ $pat && $second =~ $pat ]]
 then
