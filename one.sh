6dd14027f823e65bbc930d481ea8e1940515e43f
unknown <rahulsurana37@gmail.com>
2020-06-25 13:49:42 +0530

    Added First name Validation

--- one.sh
+++ one.sh
@@ -3,9 +3,12 @@
 shopt -s extglob
 echo "Welcome to User Registration Exercise"
 read -p "Enter First Name: " first
+function firstname(){
 if [[ $first =~ ^[[:upper:]]+([a-z]{3,}) ]]
 then
 	echo "Valid First Name";
 else
 	echo "Invalid First Name. Please Re-Enter!";
 fi
+}
+firstname
